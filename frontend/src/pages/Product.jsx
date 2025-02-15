fd15c810d47f8dba655222d0baf6b344e4b6e63b
Nirosh-Edirisinghe <nirosh1111me@gmail.com>
2025-02-14 20:45:57 -0800

    get prodct detil into array

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,6 +1,28 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import { ShopContext } from '../context/ShowContext';
 
 const Product = () => {
+  const {productId} = useParams();
+  const {products} = useContext(ShopContext);
+  const [productData, setProdductData] = useState(false);
+
+  const fetchProdctData = async ()=>{
+    products.map((item)=>{
+      if (item._id === productId){
+        setProdductData(item)
+        console.log(item);
+        
+        return null;
+      }
+    })
+  }
+
+  useEffect(()=>{
+    fetchProdctData();
+  },[productId,products])
+
+
   return (
     <div>
       
