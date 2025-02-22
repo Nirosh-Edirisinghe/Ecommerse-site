4252a53feefd9ef890255d0671ac60c511cf9acf
Nirosh-Edirisinghe <nirosh1111me@gmail.com>
2025-02-22 13:12:31 -0800

    create place order section

--- frontend/src/pages/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder.jsx
@@ -1,8 +1,12 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
 import Title from '../components/Title'
 import CartTotal from '../components/CartTotal'
+import { assets } from '../assets/assets'
+import { ShopContext } from '../context/ShowContext'
 
 const PlaceOrder = () => {
+  const [method, setMethod] = useState('cod');
+  const {navigate} = useContext(ShopContext);
   return (
     <div className='flex flex-cl sm:flex-row justify-between gap-4 pt-5 sm:pt-14 min-h-[80vh] border-t'>
       <div className="flex flex-col gap-4 w-full sm:max-w-[480px]">
@@ -37,13 +41,27 @@ const PlaceOrder = () => {
 
           {/*PAYMENT METHOD SECTION */}
           <div className="flex gap-3 flex-col lg:flex-row">
-            <div className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
-              
+            <div onClick={()=>setMethod('stripe')} className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'stripe' ? 'bg-blue-900' : ''}`}></p>
+              <img className='h-5 mx-4' src={assets.stripe_logo} alt="" />
+            </div>
+
+            <div onClick={()=>setMethod('razorpay')} className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'razorpay' ? 'bg-blue-900' : ''}`}></p>
+              <img className='h-5 mx-4' src={assets.razorpay_logo} alt="" />
+            </div>
+            <div onClick={()=>setMethod('cod')} className="flex items-center gap-3 border p-2 px-3 cursor-pointer">
+              <p className={`min-w-3.5 h-3.5 border rounded-full ${method === 'cod' ? 'bg-blue-900' : ''}`}></p>
+              <p className='text-gray-500 text-sm font-medium mx-4' >CASH ON DELVERY</p>
             </div>
           </div>
+          <div className="w-full text-end mt-8">
+        <button onClick={()=>navigate('/orders')} className="bg-black text-white px-16 py-3 text-sm">PLACE ORDER</button>
+      </div>
         </div>
       </div>
 
+      
     </div>
   )
 }
